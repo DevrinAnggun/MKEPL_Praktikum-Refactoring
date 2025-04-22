bf13f292d4c6a27be738c2c634d9dcd2af8558f8
Devrin Anggun Saputri <devrinanggunsaputri90@gmail.com>
2025-04-22 10:22:55 +0700

    Refactoring: Memisahkan class untuk di refactoring

---
+++ 08_Assessment_Tugas_Besar_Pengganti_UTS/Customer.java
@@ -0,0 +1,35 @@
+public class Customer {
+    private String nama;
+    private String nomorKTP;
+    private String nomorTelepon;
+    private String email;
+    private Gender jenisKelamin;
+
+    public Customer(String nama, String nomorKTP, String nomorTelepon, String email, Gender jenisKelamin) {
+        this.nama = nama;
+        this.nomorKTP = nomorKTP;
+        this.nomorTelepon = nomorTelepon;
+        this.email = email;
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNomorKTP() {
+        return nomorKTP;
+    }
+
+    public String getNomorTelepon() {
+        return nomorTelepon;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Gender getJenisKelamin() {
+        return jenisKelamin;
+    }
+}
