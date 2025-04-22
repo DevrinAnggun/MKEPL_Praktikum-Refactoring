ace0db9e6231744673817a4221acb8408e1cc4e8
Devrin Anggun Saputri <devrinanggunsaputri90@gmail.com>
2025-04-22 11:16:02 +0700

    Refactoring: Mengubah String menjadi enum untuk memperjelas makna data

--- 08_Assessment_Tugas_Besar_Pengganti_UTS/RoomType.java
+++ 08_Assessment_Tugas_Besar_Pengganti_UTS/RoomType.java
@@ -1,3 +1,11 @@
+//sebelum refactoring
+private String jenisKamar;
+
+public String getJenisKamar() {
+    return jenisKamar;
+}
+
+//sesudah refactoring
 public enum RoomType {
     STANDARD,
     DELUXE,
